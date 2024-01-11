df68df1a4783529e085115427706cc01d5dfcaac
skillz <goslowbutneverstop@gmail.com>
2024-01-11 00:59:22 -0500

    Cleaning up for next backup

--- script.sh
+++ script.sh
@@ -3,12 +3,16 @@
 sudo rsync -av --relative /etc/X11/xinit/xinitrc .
 sudo rsync -av --relative /home/z/.config/alacritty/ .
 sudo rsync -av --relative /home/z/.config/i3/ .
-sudo rsync -av --relative /home/z/.config/neofetch/ .
+sudo rsync -av --relative /home/z/.config/fastfetch/ .
 sudo rsync -av --relative /home/z/.config/nvim/ .
 sudo rsync -av --relative /home/z/.config/picom/ .
 sudo rsync -av --relative /home/z/.config/polybar/ .
 sudo rsync -av --relative /home/z/.zshrc .
 sudo rsync -av --relative /home/z/.config/starship.toml .
+sudo rsync -av --relative /home/z/.config/betterlockscreen/betterlockscreenrc .
+sudo rsync -av --relative /home/z/.config/dunst/dunstrc .
+sudo rsync -av --relative /home/z/.config/starship.toml .
+
 
 echo "arch dotfile updated on " $(date) >> log.txt
 
