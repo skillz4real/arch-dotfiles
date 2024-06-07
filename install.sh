292e887d4f6a5d9beda8031f28098d402c38223e
skillz <goslowbutneverstop@gmail.com>
2024-06-07 03:37:17 -0400

    first attempt to install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/bash
+
+set -x 
+
+distro=$(cat /etc/os-release | grep -ie "^id=" | cut -d '=' -f 2 )
+if [[ "$distro" == "arch" ]]; then
+  install_cmd1="pacman -S --no-confirm"
+  install_cmd2="paru -S --no-confirm"
+  dependencies="xorg-xinit xorg zsh zsh-autosuggestion zsh-syntax-highlighting kitty betterlockscreen i3 dunst fastfetch polybar picom zellij neovim nitrogen feh xborders"
+  aur_dependencies=" "
+
+  echo "Installing dependencies from Arch Repo "
+  sleep 5
+
+  `$install_cmd1 $dependencies`
+
+  echo "copying config files"
+  sleep 5
+
+  cp -a ./etc/ /etc
+  cp -a ./home/z/.config $HOME
+  cp -a ./home/z/.zshrc $HOME
+  ln -sf ./Wallpapers $HOME/Wallpapers
+   
+elif [[ "$distro" == "kali" ]]; 
+  install_cmd1="apt install -y"
+  dependencies="zsh zsh-autosuggestions zsh-syntax-highlighting kitty neofetch dunst neovim"
+  echo "Installing dependencies from debian repo"
+  `$install_cmd1 $dependencies`
+  echo "copying config files"
+  sleep 5
+  cp -a ./home/z/* $HOME
+  ln -sf ./Wallpapers $HOME/Wallpapers
+fi
--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 sudo rsync -av --relative /etc/X11/xinit/xinitrc .
-sudo rsync -av --relative /home/z/.config/alacritty/ .
 sudo rsync -av --relative /home/z/.config/i3/ .
 sudo rsync -av --relative /home/z/.config/fastfetch/ .
 sudo rsync -av --relative /home/z/.config/nvim/ .
@@ -12,6 +11,7 @@ sudo rsync -av --relative /home/z/.config/starship.toml .
 sudo rsync -av --relative /home/z/.config/betterlockscreen/betterlockscreenrc .
 sudo rsync -av --relative /home/z/.config/dunst/dunstrc .
 sudo rsync -av --relative /home/z/.config/starship.toml .
+sudo rsync -av --relative /home/z/.config/xborders .
 
 
 echo "arch dotfile updated on " $(date) >> log.txt
