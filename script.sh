ee18eebab421621b1602fc88a512c98af34b9fb0
skillz <goslowbutneverstop@gmail.com>
2024-06-09 09:28:07 -0400

    trying to port neovim config

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@
 sudo rsync -av --relative /etc/X11/xinit/xinitrc .
 sudo rsync -av --relative /home/z/.config/i3/ .
 sudo rsync -av --relative /home/z/.config/fastfetch/ .
-sudo rsync -av --relative /home/z/.config/nvim/ .
 sudo rsync -av --relative /home/z/.config/picom/ .
 sudo rsync -av --relative /home/z/.config/polybar/ .
 sudo rsync -av --relative /home/z/.zshrc .
