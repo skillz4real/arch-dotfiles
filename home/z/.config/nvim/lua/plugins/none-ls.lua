52952de9ed6d0fb1d17bbb8dda944cd3eb54afcb
skillz <goslowbutneverstop@gmail.com>
2024-07-26 02:37:57 -0400

    Fri Jul 26 02:37:57 AM EDT 2024

--- home/z/.config/nvim/lua/plugins/none-ls.lua
+++ home/z/.config/nvim/lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
 
@@ -9,7 +12,7 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
-        --null_ls.builtins.formatting.eslint_d,
+        require("none-ls.diagnostics.eslint")
       },
     })
     vim.keymap.set("n", "<leader>b", vim.lsp.buf.format, {})
